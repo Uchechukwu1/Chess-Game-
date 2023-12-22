027225965b831e21a1f38d9ce50bb7452e482339
uche onuekwusi <ucheonuekwusi1234@gmail.com>
2023-12-22 14:21:53 +0000

    function changes

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+function greeting(message) {
+  return message;
+}
+
+console.log(greeting("Hello Jason, welcome to my Chess game"));
